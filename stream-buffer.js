1960cb35388921ac2ea3671efe92b611222f0f6b
Tanvir Rudro <rudro987@gmail.com>
2024-05-16 15:51:37 +0600

    status code and error handling in node js

--- stream-buffer.js
+++ stream-buffer.js
@@ -12,13 +12,26 @@ server.on('request', (req, res) => {
         //streaming file reading
 
         // const readableStream = fs.createReadStream(__dirname + '/texts/read.txt');
+
         const readableStream = fs.createReadStream(process.cwd() + '/texts/read.txt');
+
         readableStream.on('data', (buffer) => {
+            res.statusCode = 200;
             res.write(buffer)
         });
+
         readableStream.on('end', () => {
-            res.end("Hello from Tanvir's server")
+            res.statusCode = 200;
+            res.end("The streaming is over")
         })
+
+        readableStream.on('error', (error) => {
+            console.log(error);  
+            res.statusCode = 500;
+            res.end("Something went wrong!")
+        })
+
+
     }
 
     
